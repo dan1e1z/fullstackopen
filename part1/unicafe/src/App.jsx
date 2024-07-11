abf84b6550935ba48be82f02bc237aa9e02870a7
Daniel Lindsay-Shad <danielzlindsayshad@gmail.com>
2024-07-11 21:36:42 +1000

    added part1 exercise 1.10

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,26 +12,49 @@ const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
-const Statistics = (props) => {
-  console.log(props);
+const Statistics = ({ stats }) => {
+  return (
+    <div>
+      {stats[3] > 0 ? (
+        <>
+          <StatisticLine text="good " value={stats[0]} />
+          <StatisticLine text="neutral " value={stats[1]} />
+          <StatisticLine text="bad " value={stats[2]} />
+          <StatisticLine text="all " value={stats[3]} />
+          <StatisticLine text="average " value={stats[4]} />
+          <StatisticLine text="positive " value={stats[5]} />
+        </>
+      ) : (
+        <>
+          <p>No feedback given</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+const StatisticLine = ({ text, value }) => {
+  // console.log(props);
   return (
     <>
       <p>
-        {props.text}
-        {props.state}
+        {text}
+        {value}
       </p>
     </>
   );
 };
 
 const App = () => {
-  // save clicks of each button to its own state
+  // save clicks of each button to its own value
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const all = good + neutral + bad;
   const average = (good * 1 + neutral * 0 + bad * -1) / all;
-  const positive = (good / all) * 100;
+  const positive = all ? `${(good / all) * 100} %` : "0 %";
+
+  const stats = [good, neutral, bad, all, average, positive];
 
   return (
     <div>
@@ -40,20 +63,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Header text="statistics" />
-      {!isNaN(average) ? (
-        <>
-          <Statistics text="good " state={good} />
-          <Statistics text="neutral " state={neutral} />
-          <Statistics text="bad " state={bad} />
-          <Statistics text="all " state={all} />
-          <Statistics text="average " state={average} />
-          <Statistics text="positive " state={positive} />
-        </>
-      ) : (
-        <>
-          <p>No feedback given</p>
-        </>
-      )}
+      <Statistics stats={stats} />
     </div>
   );
 };
