2d3dab04ee74043e08ac1beeb3f90893656bb4b9
Daniel Lindsay-Shad <danielzlindsayshad@gmail.com>
2024-07-11 21:50:48 +1000

    added part1 exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,31 @@
 import { useState } from "react";
 
-const Header = ({ text }) => {
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+const Statistics = ({ stats }) => {
   return (
     <>
-      <h1>{text}</h1>
+      {stats[3] > 0 ? (
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={stats[0]} />
+            <StatisticLine text="neutral" value={stats[1]} />
+            <StatisticLine text="bad" value={stats[2]} />
+            <StatisticLine text="all" value={stats[3]} />
+            <StatisticLine text="average" value={stats[4]} />
+            <StatisticLine text="positive" value={stats[5]} />
+          </tbody>
+        </table>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </>
   );
 };
@@ -12,57 +34,23 @@ const Button = (props) => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
-const Statistics = ({ stats }) => {
-  return (
-    <div>
-      {stats[3] > 0 ? (
-        <>
-          <StatisticLine text="good " value={stats[0]} />
-          <StatisticLine text="neutral " value={stats[1]} />
-          <StatisticLine text="bad " value={stats[2]} />
-          <StatisticLine text="all " value={stats[3]} />
-          <StatisticLine text="average " value={stats[4]} />
-          <StatisticLine text="positive " value={stats[5]} />
-        </>
-      ) : (
-        <>
-          <p>No feedback given</p>
-        </>
-      )}
-    </div>
-  );
-};
-
-const StatisticLine = ({ text, value }) => {
-  // console.log(props);
-  return (
-    <>
-      <p>
-        {text}
-        {value}
-      </p>
-    </>
-  );
-};
-
 const App = () => {
-  // save clicks of each button to its own value
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const all = good + neutral + bad;
-  const average = (good * 1 + neutral * 0 + bad * -1) / all;
+  const average = all ? (good * 1 + neutral * 0 + bad * -1) / all : 0;
   const positive = all ? `${(good / all) * 100} %` : "0 %";
 
   const stats = [good, neutral, bad, all, average, positive];
 
   return (
     <div>
-      <Header text="give feedback" />
+      <h1>give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <Header text="statistics" />
+      <h1>statistics</h1>
       <Statistics stats={stats} />
     </div>
   );
