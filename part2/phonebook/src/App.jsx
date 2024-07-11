da3b00806c50bd258eeffd6759b36627d3c717e2
Daniel Lindsay-Shad <danielzlindsayshad@gmail.com>
2024-07-12 02:20:55 +1000

    added part2 exercise 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const PhoneList = ({ people }) => {
+  return (
+    <div>
+      {people.map((people, i) => (
+        <div key={i}>{people.name}</div>
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <PhoneList people={persons} />
+    </div>
+  );
+};
+
+export default App;
