cd1256cd9bfb7ac2bea8518ffa89febc2b080fdb
Daniel Lindsay-Shad <danielzlindsayshad@gmail.com>
2024-07-11 19:40:04 +1000

    added part1 exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -5,14 +5,11 @@ import Total from "./components/Total";
 const App = () => {
   const course = "Half Stack application development";
 
-  const content = [
+  const parts = [
     { part: "Fundamentals of React", exercises: 10 },
     { part: "Using props to pass data", exercises: 7 },
     { part: "State of a component", exercises: 14 },
   ];
-
-  const total =
-    content[0].exercises + content[1].exercises + content[2].exercises;
 
   // const part1 = "Fundamentals of React";
   // const exercises1 = 10;
@@ -24,8 +21,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content content={content} />
-      <Total total={total} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,7 +1,10 @@
 const Total = (props) => {
+  const parts = props.parts;
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+
   return (
     <>
-      <p>Number of exercises {props.total}</p>
+      <p>Number of exercises {total}</p>
     </>
   );
 };
