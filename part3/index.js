8cd0a35aad596a8ce2103fd208957a5024b89c8a
Daniel Lindsay-Shad <danielzlindsayshad@gmail.com>
2024-07-16 19:13:21 +1000

    added part3 exercise 3.1

---
+++ part3/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0
+      ? Math.max(...persons.map((person) => Number(person.id)))
+      : 0;
+  return String(maxId + 1);
+};
+
+// app.get("/", (request, response) => {
+//   response.send("<h1>Hello World!</h1>");
+// });
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id == id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+// request (client -> server)
+// response (server -> client)
