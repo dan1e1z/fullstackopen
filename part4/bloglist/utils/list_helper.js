0ad2c71095b60106fb74addd918ca86dc8e28297
Daniel Lindsay-Shad <danielzlindsayshad@gmail.com>
2024-07-23 14:49:59 +1000

    added part4 exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,12 @@
 const dummy = (blogs) => {
- return 1; 
+  return 1;
+};
+
+const totalLikes = (blogList) => {
+  return blogList.reduce((total, item) => total + item.likes, 0);
 };
 
 module.exports = {
   dummy,
+  totalLikes,
 };
