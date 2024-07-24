d4687b7770fe12e6dc6a483a3c30dee2fa8f7365
Daniel Lindsay-Shad <danielzlindsayshad@gmail.com>
2024-07-24 14:47:58 +1000

    added part4 exercise 4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -8,52 +8,46 @@ const Blog = require("../models/blog");
 
 const initialBlogs = [
   {
-    _id: "5a422a851b54a676234d17f7",
+    id: "5a422a851b54a676234d17f7",
     title: "React patterns",
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
     likes: 7,
-    __v: 0,
   },
   {
-    _id: "5a422aa71b54a676234d17f8",
+    id: "5a422aa71b54a676234d17f8",
     title: "Go To Statement Considered Harmful",
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
-    __v: 0,
   },
   {
-    _id: "5a422b3a1b54a676234d17f9",
+    id: "5a422b3a1b54a676234d17f9",
     title: "Canonical string reduction",
     author: "Edsger W. Dijkstra",
     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
     likes: 12,
-    __v: 0,
   },
   {
-    _id: "5a422b891b54a676234d17fa",
+    id: "5a422b891b54a676234d17fa",
     title: "First class tests",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
     likes: 10,
-    __v: 0,
   },
   {
-    _id: "5a422ba71b54a676234d17fb",
+    id: "5a422ba71b54a676234d17fb",
     title: "TDD harms architecture",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
     likes: 0,
-    __v: 0,
   },
   {
-    _id: "5a422bc61b54a676234d17fc",
+    id: "5a422bc61b54a676234d17fc",
     title: "Type wars",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 2,
-    __v: 0,
   },
 ];
 
@@ -67,17 +61,26 @@ beforeEach(async () => {
   );
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+  test("there are two notes", async () => {
+    const response = await api.get("/api/blogs");
+    assert.strictEqual(response.body.length, initialBlogs.length);
+  });
 
-test("there are two notes", async () => {
-  const response = await api.get("/api/blogs");
-  assert.strictEqual(response.body.length, initialBlogs.length);
-});
+  test("unique identifier property of the blog posts is named id", async () => {
+    const response = await api.get("/api/blogs");
+    const blogs = response.body;
+
+    blogs.forEach((blog) => {
+      assert(blog.id !== undefined, "Blog should have an id property");
+      assert(blog._id === undefined, "Blog should not have an _id property");
+    });
+  });
 
 after(async () => {
   await mongoose.connection.close();
