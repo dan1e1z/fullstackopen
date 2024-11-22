a31dff80f7e98e10a474df15fe2a9376d75413b0
Daniel Lindsay-Shad <danielzlindsayshad@gmail.com>
2024-11-23 01:31:24 +1100

    updated part6 exercise 6.9-6.24

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,31 +1,33 @@
 import { useDispatch, useSelector } from "react-redux";
 import { addVote } from "../reducers/anecdoteReducer";
 import { displayNotification } from "../reducers/notificationReducer";
+import { useMemo, useCallback } from "react";
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => {
-    console.log(state);
-    if (state.filter === "") {
-      return state.anecdotes;
-    }
-    return state.anecdotes.filter((anecdote) =>
-      anecdote.content.toLowerCase().includes(state.filter.toLowerCase()),
-    );
-  });
-
-  console.log("anecdotes is", anecdotes);
+  const { anecdotes, filter } = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  const vote = (id) => {
-    const anecdote = anecdotes.find((a) => a.id === id);
-    console.log("anecdote is", anecdote);
-    dispatch(addVote(id));
-    dispatch(displayNotification(`you voted '${anecdote.content}' !`, 5));
-  };
+  // Memoize filtered anecdotes to prevent recalculation on every render
+  const filteredAnecdotes = useMemo(() => {
+    if (filter === "") return anecdotes;
+    return anecdotes.filter((anecdote) =>
+      anecdote.content.toLowerCase().includes(filter.toLowerCase()),
+    );
+  }, [anecdotes, filter]);
+
+  // Memoize the vote function to avoid re-creating it on every render
+  const vote = useCallback(
+    (id) => {
+      const anecdote = anecdotes.find((a) => a.id === id);
+      dispatch(addVote(id));
+      dispatch(displayNotification(`you voted '${anecdote.content}' !`, 5));
+    },
+    [dispatch, anecdotes],
+  );
 
   return (
     <div>
-      {[...anecdotes]
+      {[...filteredAnecdotes]
         .sort((a, b) => b.votes - a.votes)
         .map((anecdote) => (
           <div key={anecdote.id}>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,31 +1,12 @@
-import { createSlice, current } from "@reduxjs/toolkit";
-
+import { createSlice } from "@reduxjs/toolkit";
 import anecdoteService from "../services/anecdotes";
 
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
-
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
+// Initial state is fetched from the backend, so no need for hardcoded data
+const initialState = [];
 
 const anecdoteSlice = createSlice({
   name: "anecdotes",
-  initialState: [],
+  initialState,
   reducers: {
     appendAnecdote(state, action) {
       state.push(action.payload);
@@ -34,11 +15,11 @@ const anecdoteSlice = createSlice({
       return action.payload;
     },
     updateAnecdote(state, action) {
-      const id = action.payload.id;
-      const changedAnecdote = action.payload;
-      return state.map((anecdote) =>
-        anecdote.id !== id ? anecdote : changedAnecdote,
-      );
+      const { id, votes } = action.payload;
+      const index = state.findIndex((anecdote) => anecdote.id === id);
+      if (index !== -1) {
+        state[index] = { ...state[index], votes }; // Update the anecdote directly
+      }
     },
   },
 });
@@ -61,15 +42,14 @@ export const createAnecdote = (content) => {
 };
 
 export const addVote = (id) => {
-  return async (dispatch) => {
-    const anecdotes = await anecdoteService.getAll();
-    const anecdoteToChange = anecdotes.find((a) => a.id === id);
+  return async (dispatch, getState) => {
+    const anecdote = getState().anecdotes.find((a) => a.id === id); // Get from state
     const changedAnecdote = {
-      ...anecdoteToChange,
-      votes: anecdoteToChange.votes + 1,
+      ...anecdote,
+      votes: anecdote.votes + 1,
     };
-    const response = await anecdoteService.update(id, changedAnecdote);
-    dispatch(updateAnecdote(response));
+    const updatedAnecdote = await anecdoteService.update(id, changedAnecdote);
+    dispatch(updateAnecdote(updatedAnecdote));
   };
 };
 
